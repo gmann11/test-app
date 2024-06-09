da2c3c53e3a6f3e7c25ec36c2bd1479cd368c88c
Gary Mann <gmann11@users.noreply.github.com>
2024-06-08 21:06:01 -0400

    Update for Bloom 2.11.0

--- sharestore.sh
+++ sharestore.sh
@@ -5,5 +5,10 @@
 # redux store to window.store.
 # usage: ./sharestore.sh <path to index-xxxxxx.js file>
 # v0.1 (02/06/2024)
-#
-perl -i.bkp -pe 's/(\(\{reducer:[^,]+,middleware:.*?);return\s([a-zA-z]+)/${1};window.store=$2;return $2/' $1
+# v0.2 (06/07/2024) - for Bloom 2.11.0
+
+# for Bloom 2.10.0
+# perl -i.bkp -pe 's/(\(\{reducer:[^,]+,middleware:.*?);return\s([a-zA-z]+)/${1};window.store=$2;return $2/' $1
+
+# for Bloom 2.11.0
+perl -i.bak -pe 's/;([^\.]+\.useEffect\(\(\)=>{[^\(]+\(\),[^\(]+\(\),[^\(]+\(([^(]+)\)})/$1,window.store=$2,console.log("Exposing store:",$2)/' $1
